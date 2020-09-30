9f5d31be8762233fbae9a1935fc419892cb48166
Xuan <1262610086@qq.com>
2020-09-30 15:39:18 +0800

    增加了截图调起按钮后，五秒后按钮自动隐藏的功能

--- app/src/main/java/com/example/screenshotlistentest/activity/MainActivity.java
+++ app/src/main/java/com/example/screenshotlistentest/activity/MainActivity.java
@@ -12,6 +12,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,8 +26,12 @@ import com.example.screenshotlistentest.fragment.Page2Fragment;
 import com.example.screenshotlistentest.R;
 import com.example.screenshotlistentest.manager.ScreenShotListenManager;
 
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity {
     public ScreenShotListenManager manager;
+    MainHandler mainHandler = new MainHandler(MainActivity.this);
+    CountTimeThread countTimeThread;
 
     private RadioGroup radioGroup;
     private Button getHelp;
@@ -35,6 +41,78 @@ public class MainActivity extends AppCompatActivity {
     private Page2Fragment page2Fragment;
 
     private int mIndex;
+
+    private static class MainHandler extends Handler{
+        private final int MSG_HIDE = 1;
+        private final WeakReference<MainActivity> weakRef;
+
+        public MainHandler(MainActivity pAcitivity){
+            weakRef = new WeakReference<MainActivity>(pAcitivity);
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            final MainActivity mainActivity = weakRef.get();
+            if(mainActivity != null){
+                switch (msg.what){
+                    case MSG_HIDE:
+                        mainActivity.hide();
+                        break;
+                }
+            }
+            super.handleMessage(msg);
+        }
+
+        /**
+         * 发送信息
+         */
+        public void sendHideControlMessage(){
+            obtainMessage(MSG_HIDE).sendToTarget();
+        }
+    }
+
+
+    private class CountTimeThread extends Thread{
+        private final long maxVisibleTime;
+        private long startVisibleTime;
+
+        public CountTimeThread(int second){
+            maxVisibleTime = second * 1000;
+
+            //设置为后台进程
+            setDaemon(true);
+        }
+
+        private synchronized void reset(){
+            startVisibleTime = System.currentTimeMillis();
+        }
+
+        @Override
+        public void run() {
+            startVisibleTime = System.currentTimeMillis();
+
+            while (true){
+                if((startVisibleTime + maxVisibleTime) < System.currentTimeMillis()){
+                    mainHandler.sendHideControlMessage();
+                }
+            }
+        }
+    }
+
+    /**
+     * 输入定时显示时间，开启线程
+     */
+    private void startCountTimeThread(){
+        countTimeThread = new CountTimeThread(5);
+        countTimeThread.start();
+    }
+
+    /**
+     * 隐藏按钮
+     */
+    private void hide(){
+        getHelp.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,20 +130,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case 1:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-                }else {
-                    listenScreenShot();
-                }
-                break;
-        }
-    }
-
-
+    /**
+     * 对主活动界面进行初始化
+     */
     private void initView(){
         getHelp = (Button)findViewById(R.id.getHelp);
         getHelp.setOnClickListener(new View.OnClickListener() {
@@ -90,6 +157,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * 设置初始碎片
+     */
     private void initFragment(){
         page1Fragment = new Page1Fragment();
         page2Fragment = new Page2Fragment();
@@ -101,6 +171,9 @@ public class MainActivity extends AppCompatActivity {
         setIndexSelected(0);
     }
 
+    /**
+     *主活动两个碎片的切换
+     */
     private void setIndexSelected(int index){
         if(mIndex == index){
             return;
@@ -120,6 +193,9 @@ public class MainActivity extends AppCompatActivity {
         mIndex = index;
     }
 
+    /**
+     * 截屏监听接口，实现具体逻辑
+     */
     protected void listenScreenShot(){
         manager.setListener(new ScreenShotListenManager.OnScreenShotListener() {
             @Override
@@ -127,13 +203,33 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"截图监听成功，图片路径为：" + imagePath,Toast.LENGTH_SHORT).show();
                 Log.d("MainActivity.class","imagePath:" + imagePath);
                 getHelp.setVisibility(View.VISIBLE);
+                startCountTimeThread();
             }
         });
     }
 
+    /**
+     * 结束活动时结束截屏监听
+     */
     @Override
     protected void onDestroy() {
         manager.stopListen();
         super.onDestroy();
+    }
+
+    /**
+     *确认权限
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                }else {
+                    listenScreenShot();
+                }
+                break;
+        }
     }
 }
